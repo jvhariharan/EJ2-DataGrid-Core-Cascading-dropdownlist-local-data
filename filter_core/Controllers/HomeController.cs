8fb334682196bb8f6ce13fc961d6d1b4d24feffd
manivel-sellamuthu <manivel-sellamuthu@users.noreply.github.com>
2019-12-10 18:51:33 +0530

    Sample added for core cascading dropdownlist

---
+++ filter_core/Controllers/HomeController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using filter_core.Models;
+
+namespace TestSample.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var Order = OrdersDetails.GetAllRecords();
+            ViewBag.DataSource = Order;
+            return View();
+        }
+
+        public IActionResult UrlDatasource([FromBody]Data dm)
+        {
+            var order = OrdersDetails.GetAllRecords();
+            var Data = order.ToList();
+            int count = order.Count();
+            return dm.requiresCounts ? Json(new { result = Data.Skip(dm.skip).Take(dm.take), count = count }) : Json(Data);
+        }
+
+        public ActionResult Update([FromBody]CRUDModel<OrdersDetails> value)
+        {
+            var ord = value.value;
+            OrdersDetails val = OrdersDetails.GetAllRecords().Where(or => or.OrderID == ord.OrderID).FirstOrDefault();
+            val.OrderID = ord.OrderID;
+            val.EmployeeID = ord.EmployeeID;
+            val.CustomerID = ord.CustomerID;
+            val.Freight = ord.Freight;
+            val.ShipCountry = ord.ShipCountry;
+            val.ShipCity = ord.ShipCity;
+
+            return Json(value.value);
+        }
+        //insert the record
+        public ActionResult Insert([FromBody]CRUDModel<OrdersDetails> value)
+        {
+
+            OrdersDetails.GetAllRecords().Insert(0, value.value);
+            return Json(value.value);
+        }
+        //Delete the record
+        public ActionResult Delete([FromBody]CRUDModel<OrdersDetails> value)
+        {
+            OrdersDetails.GetAllRecords().Remove(OrdersDetails.GetAllRecords().Where(or => or.OrderID == int.Parse(value.key.ToString())).FirstOrDefault());
+            return Json(value);
+        }
+
+
+        public class Data
+        {
+
+            public bool requiresCounts { get; set; }
+            public int skip { get; set; }
+            public int take { get; set; }
+        }
+        public class CRUDModel<T> where T : class
+        {
+            public string action { get; set; }
+
+            public string table { get; set; }
+
+            public string keyColumn { get; set; }
+
+            public object key { get; set; }
+
+            public T value { get; set; }
+
+            public List<T> added { get; set; }
+
+            public List<T> changed { get; set; }
+
+            public List<T> deleted { get; set; }
+
+            public IDictionary<string, object> @params { get; set; }
+        }
+    }
+    public class OrdersDetails
+    {
+        public static List<OrdersDetails> order = new List<OrdersDetails>();
+        public OrdersDetails()
+        {
+
+        }
+        public OrdersDetails(int OrderID, string CustomerId, int EmployeeId, double Freight, bool Verified, DateTime OrderDate, string ShipCity, string ShipName, string ShipCountry, DateTime ShippedDate, string ShipAddress)
+        {
+            this.OrderID = OrderID;
+            this.CustomerID = CustomerId;
+            this.EmployeeID = EmployeeId;
+            this.Freight = Freight;
+            this.ShipCity = ShipCity;
+            this.Verified = Verified;
+            this.OrderDate = OrderDate;
+            this.ShipName = ShipName;
+            this.ShipCountry = ShipCountry;
+            this.ShippedDate = ShippedDate;
+            this.ShipAddress = ShipAddress;
+        }
+        public static List<OrdersDetails> GetAllRecords()
+        {
+            if (order.Count() == 0)
+            {
+                int code = 10000;
+                for (int i = 1; i < 10; i++)
+                {
+                    order.Add(new OrdersDetails(code + 1, "ALFKI", i + 0, 2.3 * i, false, new DateTime(1991, 05, 15), "1", "New York", "United States", new DateTime(1996, 7, 16), "Kirchgasse 6"));
+                    order.Add(new OrdersDetails(code + 2, "ANATR", i + 2, 3.3 * i, true, new DateTime(1990, 04, 04), "2", "Queensland", "Australia", new DateTime(1996, 9, 11), "Avda. Azteca 123"));
+                    order.Add(new OrdersDetails(code + 3, "ANTON", i + 1, 4.3 * i, true, new DateTime(1957, 11, 30), "1", "Virginia", "United States", new DateTime(1996, 10, 7), "Carrera 52 con Ave. Bolívar #65-98 Llano Largo"));
+                    order.Add(new OrdersDetails(code + 4, "BLONP", i + 3, 5.3 * i, false, new DateTime(1930, 10, 22), "1", "Washington", "United States", new DateTime(1996, 12, 30), "Magazinweg 7"));
+                    order.Add(new OrdersDetails(code + 5, "BOLID", i + 4, 6.3 * i, true, new DateTime(1953, 02, 18), "2", "Tasmania", "Australia", new DateTime(1997, 12, 3), "1029 - 12th Ave. S."));
+                    order.Add(new OrdersDetails(code + 6, "VINET", i + 5, 7.3 * i, true, new DateTime(1969, 02, 18), "2", "Victoria", "Australia", new DateTime(1997, 12, 3), "7 th Avenue"));
+                    code += 5;
+                }
+            }
+            return order;
+        }
+
+        public int? OrderID { get; set; }
+        public string CustomerID { get; set; }
+        public int? EmployeeID { get; set; }
+        public double? Freight { get; set; }
+        public string ShipCity { get; set; }
+        public bool Verified { get; set; }
+        public DateTime OrderDate { get; set; }
+
+        public string ShipName { get; set; }
+
+        public string ShipCountry { get; set; }
+
+        public DateTime ShippedDate { get; set; }
+        public string ShipAddress { get; set; }
+
+    }
+}
